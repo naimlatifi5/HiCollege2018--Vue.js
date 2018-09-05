ae42739ec7fb2177c8bc38013e46e5629f5cac5d
Naim Latifi <naimlatifi98@hotmail.com>
2018-09-05 22:47:35 +0200

    installed sass loader, imported inside app , downloaded bootstrap, added mocked template for rsvp app

--- hicollege-vuejs/src/components/HelloWorld.vue
+++ hicollege-vuejs/src/components/HelloWorld.vue
@@ -1,6 +1,74 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <div class="container">
+        <h1>RSVP- APP <span class="badge badge-secondary badge-secondary--color">HiCollege HIQ</span></h1>
+        <div class="progress">
+            <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">25%</div>
+        </div>
+          <h2>Vue.js HiCollege course</h2>
+          <p>
+            Vue is becoming extremely popular JavaScript framework nowadays which lets you build dynamic and interactive web apps in a simple way. The secret of Vue.js framework is its simplicity, however to help you get started with the framework I built this course to get you familiar with the core concepts of the framework.
+
+          </p>
+          <hr>
+          <div class="row">
+            <div class="col-6">
+              <h5>55 people attending</h5>
+            </div>
+            <div class="col-6">
+              <button type="button" class="btn btn-primary btn-lg">I will attend</button>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-12">
+              <form class="rsvp-form">
+                  <div class="form-group">
+                    <label for="exampleInputEmail1">Name</label>
+                    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter name">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleEmail">Email</label>
+                    <input type="email" class="form-control" id="exampleEmail" placeholder="Email">
+                  </div>
+                  <div class="form-group">
+                    <label for="comment">Comment</label>
+                    <input type="text" class="form-control" id="comment" placeholder="Enter commment">
+                  </div>
+                  <button type="submit" class="btn btn-primary">Submit</button>
+                  <button type="button" class="btn btn-primary">Cancel</button>
+                </form>
+            </div>
+          </div>
+          <div class="row">
+            <table class="table table-hover">
+              <thead>
+                <tr>
+                  <th scope="col">Name</th>
+                  <th scope="col">Comment</th>
+                  <th scope="col">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Mark</td>
+                  <td>Otto</td>
+                  <td><button type="button" class="btn btn-danger">Danger</button></td>
+                </tr>
+                <tr>
+                  <td>Jacob</td>
+                  <td>Thornton</td>
+                  <td><button type="button" class="btn btn-danger">Danger</button></td>
+                </tr>
+                <tr>
+                  <td>Jacob</td>
+                  <td>Thorenor</td>
+                  <td><button type="button" class="btn btn-danger">Danger</button></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+    </div>
 
   </div>
 </template>
