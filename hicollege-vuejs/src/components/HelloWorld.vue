7d33c82a91817a18f2a174ce5f3c9ea48be3d81a
Naim Latifi <naimlatifi98@hotmail.com>
2018-09-05 23:00:03 +0200

    styling up our rsvp app

--- hicollege-vuejs/src/components/HelloWorld.vue
+++ hicollege-vuejs/src/components/HelloWorld.vue
@@ -5,41 +5,58 @@
         <div class="progress">
             <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">25%</div>
         </div>
-          <h2>Vue.js HiCollege course</h2>
-          <p>
-            Vue is becoming extremely popular JavaScript framework nowadays which lets you build dynamic and interactive web apps in a simple way. The secret of Vue.js framework is its simplicity, however to help you get started with the framework I built this course to get you familiar with the core concepts of the framework.
-
-          </p>
-          <hr>
-          <div class="row">
-            <div class="col-6">
-              <h5>55 people attending</h5>
+        <div class="row">
+          <div class="header section">
+            <div class="section__header">
+                <h2>Vue.js HiCollege course</h2>
             </div>
-            <div class="col-6">
-              <button type="button" class="btn btn-primary btn-lg">I will attend</button>
+            <div class="section__content">
+              <p>
+                Vue is becoming extremely popular JavaScript framework nowadays which lets you build dynamic and interactive web apps in a simple way. The secret of Vue.js framework is its simplicity, however to help you get started with the framework I built this course to get you familiar with the core concepts of the framework.
+              </p>
+            </div>
+          </div>
+        </div>
+
+
+
+
+          <div class="row section">
+            <div class="section__header">
+              <div class="col-6">
+                <h5>55 people attending</h5>
+              </div>
+              <div class="col-6">
+                <button type="button" class="btn btn-primary btn-lg">I will attend</button>
+              </div>
+            </div>
+            <div class="section__content">
+              <div class="row">
+                <div class="col-12">
+                  <form class="rsvp-form">
+                      <div class="form-group">
+                        <label for="exampleInputEmail1">Name</label>
+                        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter name">
+                      </div>
+                      <div class="form-group">
+                        <label for="exampleEmail">Email</label>
+                        <input type="email" class="form-control" id="exampleEmail" placeholder="Email">
+                      </div>
+                      <div class="form-group">
+                        <label for="comment">Comment</label>
+                        <input type="text" class="form-control" id="comment" placeholder="Enter commment">
+                      </div>
+                      <button type="submit" class="btn btn-primary">Submit</button>
+                      <button type="button" class="btn btn-primary">Cancel</button>
+                    </form>
+                </div>
+              </div>
             </div>
           </div>
 
-          <div class="row">
-            <div class="col-12">
-              <form class="rsvp-form">
-                  <div class="form-group">
-                    <label for="exampleInputEmail1">Name</label>
-                    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter name">
-                  </div>
-                  <div class="form-group">
-                    <label for="exampleEmail">Email</label>
-                    <input type="email" class="form-control" id="exampleEmail" placeholder="Email">
-                  </div>
-                  <div class="form-group">
-                    <label for="comment">Comment</label>
-                    <input type="text" class="form-control" id="comment" placeholder="Enter commment">
-                  </div>
-                  <button type="submit" class="btn btn-primary">Submit</button>
-                  <button type="button" class="btn btn-primary">Cancel</button>
-                </form>
-            </div>
-          </div>
+
+
+
           <div class="row">
             <table class="table table-hover">
               <thead>
