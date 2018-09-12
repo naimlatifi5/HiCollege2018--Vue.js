d425ccf41bae5f22230e07c15fd5e7ea5f0a61fe
Naim Latifi <naimlatifi98@hotmail.com>
2018-09-12 21:24:40 +0200

    added data binding

--- hicollege-vuejs/src/components/vue-essentials/dataBinding.vue
+++ hicollege-vuejs/src/components/vue-essentials/dataBinding.vue
@@ -1,12 +1,22 @@
 <template>
   <div class="dataBinding">
     <h1>Hello Two way data binding</h1>
+
+    <p>
+Show the question: {{question}}
+    </p>
+    <input v-model="question" />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'dataBinding'
+  name: 'dataBinding',
+  data () {
+    return {
+      question: 'What is your question ? '
+    }
+  }
 }
 </script>
 
