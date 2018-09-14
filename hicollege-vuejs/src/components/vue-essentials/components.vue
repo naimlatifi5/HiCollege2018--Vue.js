891ff67c6f969e353c5592408ae3464b844d276c
Naim Latifi <naimlatifi98@hotmail.com>
2018-09-14 22:35:14 +0200

    more about components and rendering

--- hicollege-vuejs/src/components/vue-essentials/components.vue
+++ hicollege-vuejs/src/components/vue-essentials/components.vue
@@ -1,12 +1,33 @@
 <template>
   <div class="components">
     <h1>Hello components</h1>
+    <first-section></first-section>
+    <hr />
+    <second-section></second-section>
+    <hr />
+    <first-section></first-section>
+    <hr />
+    <first-section></first-section>
   </div>
 </template>
 
 <script>
+// importing the components
+import firstSection from './components/firstSection.vue';
+import secondSection from './components/secondSection.vue';
+
 export default {
-  name: 'components'
+  name: 'components',
+  data () {
+    return {
+
+    }
+  },
+  // component registration
+  components: {
+    'first-section' : firstSection,
+    'second-section': secondSection
+  }
 }
 </script>
 
--- hicollege-vuejs/src/components/vue-essentials/listConditionalRendering.vue
+++ hicollege-vuejs/src/components/vue-essentials/listConditionalRendering.vue
@@ -1,14 +1,52 @@
 <template>
   <div class="listRendering">
     <h1>Hello list rendering</h1>
+    <ul class="list-group" v-for="(item, index) in hiCollegeList">
+      <li class="list-group-item" :id="index">{{item.name}} - {{item.foodpreferernce}}</li>
+  </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'listConditionalRendering'
+  name: 'listConditionalRendering',
+  data () {
+    return {
+      hiCollegeList: [
+        {
+          name: 'Naim Latifi',
+          foodpreferernce: 'No pigg meat'
+        },
+        {
+          name: 'Leanne Graham',
+          foodpreferernce: 'Veg'
+        },
+        {
+          name: 'Ervin Howell',
+          foodpreferernce: 'Veg'
+        },
+        {
+          name: 'Clementine Bauch',
+          foodpreferernce: 'Veg'
+        },
+        {
+          name: 'Patricia Lebsack',
+          foodpreferernce: 'Veg'
+        }
+      ]
+    }
+  }
 }
 </script>
 
 <style lang="scss">
+  .list-items {
+    li {
+      margin-bottom: 20px;
+
+    }
+    span {
+      display: block;
+    }
+  }
 </style>
