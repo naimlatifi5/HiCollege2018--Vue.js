a2be079f050d479498aee324ec556f43b34f3f2c
Naim Latifi <naimlatifi98@hotmail.com>
2018-09-24 21:58:57 +0200

    More enhancement on components communication

--- hicollege-vuejs/src/components/vue-essentials/components.vue
+++ hicollege-vuejs/src/components/vue-essentials/components.vue
@@ -1,32 +1,19 @@
 <template>
   <div class="components">
     <h1>Hello components</h1>
-    <first-section></first-section>
-    <hr />
-    <second-section></second-section>
-    <hr />
-    <first-section></first-section>
-    <hr />
-    <first-section></first-section>
+    <parent-component></parent-component>
   </div>
 </template>
 
 <script>
 // importing the components
-import firstSection from './components/firstSection.vue'
-import secondSection from './components/secondSection.vue'
+import parentComponent from './components/parentComponent.vue'
 
 export default {
   name: 'components',
-  data () {
-    return {
-
-    }
-  },
   // component registration
   components: {
-    'first-section': firstSection,
-    'second-section': secondSection
+    'parent-component': parentComponent
   }
 }
 </script>
---
+++ hicollege-vuejs/src/components/vue-essentials/components/parentComponent.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="parent-component">
+    <h3 :style="{fontSize: fontSize + 'px', color: color}" >Hello parent</h3>
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum </p>
+
+    <child-component @changeColor="changeMyColor"></child-component>
+
+  </div>
+</template>
+
+<script>
+import childComponent from './childComponent.vue'
+export default {
+  name: 'parentComponent',
+  data () {
+    return {
+      fontSize: 30,
+      color: 'green'
+    }
+  },
+  methods: {
+    changeMyColor (color) {
+      this.fontSize = 50;
+      this.color = color;
+    }
+  },
+  components: {
+    'child-component': childComponent
+  }
+}
+</script>
+
+<style lang="scss">
+.parent-component {
+  padding: 20px;
+  border: 1px solid grey;
+  background-color: white;
+}
+</style>
